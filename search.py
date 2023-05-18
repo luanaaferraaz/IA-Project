a72ef0d4cf7d70f6b5c8eed8b28c29e8c58ed676
luanaaferraaz <109438076+luanaaferraaz@users.noreply.github.com>
2023-05-18 16:26:12 +0100

    search

--- search.py
+++ search.py
@@ -204,9 +204,9 @@ def depth_first_tree_search(problem):
     """
 
     frontier = [Node(problem.initial)]  # Stack
+
     while frontier:
         node = frontier.pop()
-        print(type(node.state))
         if problem.goal_test(node.state):
             return node
         frontier.extend(node.expand(problem))
